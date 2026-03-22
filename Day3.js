25a3de46cbabdd1cb9a8e527458b987a7c50a5f2
Mitali Gohel <114275021+mitali2003@users.noreply.github.com>
2026-03-22 08:45:38 +0530

    Add files via upload

---
+++ Day3.js
@@ -0,0 +1,44 @@
+// *
+// **
+// ***
+// ****
+// *****
+
+// const printStar = (n) => {
+//   for (let i = 1; i <= n; i++) {
+//     let str = "";
+//     for (let j = 1; j <= i; j++) {
+//       str = str + "*";
+//     }
+//     console.log(str);
+//   }
+// };
+// printStar(5);
+
+// *****
+// ****
+// ***
+// **
+// *
+
+// const reverseStar = (n) => {
+//   for (let i = n; i >= 1; i--) {
+//     let str = "";
+//     for (j = i; j >= 1; j--) {
+//       str = str + "*";
+//     }
+//     console.log(str);
+//   }
+// };
+// reverseStar(3);
+
+//     *
+//    ***
+//   *****
+//  *******
+// *********
+
+const printPryamid = (n) => {
+    
+};
+printPryamid(5);
